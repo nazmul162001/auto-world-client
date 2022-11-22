5acf5f2df734bfbf0861ecc2fcaac70090adb2de
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-11-23 00:13:45 +0600

    install all dependency

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Hello Bangladesh</h1>
+      <h1 className='text-3xl font-bold text-center underline'>Hello world!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
