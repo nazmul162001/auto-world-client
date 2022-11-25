1467a51702c06a09dfcebc61d4a22bce2e29c864
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-11-25 17:14:01 +0600

    completed car details page

--- src/components/CarDetailsModal/CarDetailsModal.js
+++ src/components/CarDetailsModal/CarDetailsModal.js
@@ -5,8 +5,9 @@ import {
   Dialog,
   DialogHeader,
   DialogBody,
-  DialogFooter,
 } from '@material-tailwind/react'
+import { AiOutlineClose } from 'react-icons/ai'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const CarDetailsModal = ({ size, handleOpen, carInfo }) => {
   const {
@@ -25,30 +26,51 @@ const CarDetailsModal = ({ size, handleOpen, carInfo }) => {
     <>
       <Fragment>
         <Dialog open={size === 'xxl'} size={size || 'md'} handler={handleOpen}>
-          <DialogHeader>Its a simple dialog.</DialogHeader>
-          <DialogBody divider>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-            ad reprehenderit omnis perspiciatis aut odit! Unde architecto
-            perspiciatis, dolorum dolorem iure quia saepe autem accusamus eum
-            praesentium magni corrupti explicabo!
+          <DialogHeader className='text-lg md:text-xl flex justify-center items-center w-full text-softBlack relative'>
+            More Details About Your Dream Car
+          </DialogHeader>
+          <span
+            onClick={() => handleOpen(null)}
+            className='absolute top-3 right-5 p-2 bg-secondary text-primary rounded-full cursor-pointer'
+          >
+            <AiOutlineClose className='text-xl' />
+          </span>
+          <DialogBody className='grid grid-cols-1 md:grid-cols-2' divider>
+            <div className='p-3'>
+              <img
+                className='rounded w-full h-auto md:h-[350px]'
+                src={img}
+                alt=''
+              />
+            </div>
+            <div className='p-3 leading-10'>
+              <div>
+                <h3 className='font-bold'>
+                  Posted: <span className='text-gray mx-3'>{createdDay}</span>
+                  {createdTime}
+                </h3>
+                <h3 className='font-bold'>
+                  Car Name <span className='text-gray ml-3'>{carName}</span>
+                </h3>
+                <h3 className='font-bold'>
+                  Mileage: <span className='text-gray ml-3'>{mileage}</span>
+                </h3>
+                <h3 className='font-bold'>
+                  Location: <span className='text-gray ml-3'>{location}</span>
+                </h3>
+                <h3 className='font-bold'>
+                  Engine: <span className='text-gray ml-3'>{engine}</span>
+                </h3>
+                <h3 className='font-bold'>
+                  Transmission: <span className='text-gray ml-3'>{transmission}</span>
+                </h3>
+                <h3 className='font-bold'>
+                  Price: $<span className='text-gray ml-3'>{price}</span>
+                </h3>
+              </div>
+              <Button className='my-5 bg-secondary text-primary font-bold text-lg capitalize flex justify-center items-center' fullWidth>Make Purchase <FaLocationArrow className='ml-2' /> </Button>
+            </div>
           </DialogBody>
-          <DialogFooter>
-            <Button
-              variant='text'
-              color='red'
-              onClick={() => handleOpen(null)}
-              className='mr-1'
-            >
-              <span>Cancel</span>
-            </Button>
-            <Button
-              variant='gradient'
-              color='green'
-              onClick={() => handleOpen(null)}
-            >
-              <span>Confirm</span>
-            </Button>
-          </DialogFooter>
         </Dialog>
       </Fragment>
     </>
